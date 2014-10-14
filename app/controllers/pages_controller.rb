a75489285e264f8cba83a534dbfd1a577b433f5b
lbhasemeyer <lbhasemeyer@gmail.com>
2014-10-14 15:30:29 -0600

    moved array to pages controller from index, noted out in index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
 
+def index
+  @quotes= [
+  ["We owe a lot to Thomas Edison - if it wasn't for him, we'd be watching television by candlelight.", "Milton Berle"],
+  ["The higher a monkey climbs, the more you see of its behind.", "Joseph Stilwell"],
+  ["I think it's wrong that only one company makes the game Monopoly.", "Steven Wright"]
+  ]
+end
+
 end
