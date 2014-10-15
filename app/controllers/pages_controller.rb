37df796796db3a2fdcf8178b8a23c4441e826770
lbhasemeyer <lbhasemeyer@gmail.com>
2014-10-15 14:13:01 -0600

    replaced array of strings with array of Quote objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController < ApplicationController
 
 def index
-  @quotes= [
-  ["We owe a lot to Thomas Edison - if it wasn't for him, we'd be watching television by candlelight.", "Milton Berle"],
-  ["The higher a monkey climbs, the more you see of its behind.", "Joseph Stilwell"],
-  ["I think it's wrong that only one company makes the game Monopoly.", "Steven Wright"]
-  ]
+
+  quote1 = Quote.new
+  quote1.text = "We owe a lot to Thomas Edison - if it wasn't for him, we'd be watching television by candlelight."
+  quote1.author = "Al Boliska"
+
+  quote2 = Quote.new
+  quote2.text = "The higher a monkey climbs, the more you see of its behind."
+  quote2.author = "Joseph Stilwell"
+
+  quote3 = Quote.new
+  quote3.text = "I think it's wrong that only one company makes the game Monopoly."
+  quote3.author = "Steven Wright"
+
+@quotes= [quote1, quote2, quote3]
+
 end
 
 end
