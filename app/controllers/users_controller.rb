431a71832bd84a72c4913ea0a34923415b0997df
lbhasemeyer <lbhasemeyer@gmail.com>
2014-10-27 14:54:06 -0600

    added users page and functionality to add first name, last name, and email - next: making it pretty

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024213727) do
+ActiveRecord::Schema.define(version: 20141027202329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20141024213727) do
     t.datetime "updated_at"
     t.boolean  "complete",    default: false
     t.date     "due_date"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
