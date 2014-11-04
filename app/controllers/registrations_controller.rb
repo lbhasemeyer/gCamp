be3858d74e4435a399191c51394b69ad1d814355
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-04 13:06:48 -0700

    added params from the last commit - password stuff to users controller and names to registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(params.require(:user).
-        permit(:email, :password, :password_confirmation))
+        permit(:first_name, :last_name, :email, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
