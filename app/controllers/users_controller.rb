2899d65e97c3575bc023ec5be5a8d5aa8010b84a
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-10 11:41:16 -0700

    required fields to be filled out for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    respond_to do |format|
-      @user.save
-      format.html { redirect_to users_path, notice: 'User was successfully created.' }
+    @user = User.new(params.require(:user).
+        permit(:first_name, :last_name, :email, :password, :password_confirmation))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
