0f459803983f4c3d549e1bb3c27d16c68f933fbc
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-11 15:58:01 -0700

    working on model specs, made it possible to edit task with due date in past

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -6,28 +6,32 @@ describe Task do
     task = Task.new
     expect(task.valid?).to be(false)
     task.description = 'Eat Cheetos'
-    expect(task.valid?).to be(false)
-    task.due_date = '12/12/3014'
     expect(task.valid?).to be(true)
   end
 
   it 'verifies date is today or after' do
     task = Task.new
     task.description = 'Eat Cheetos'
+    task.due_date = '10/12/2001'
     expect(task.valid?).to be(false)
     task.due_date = '12/12/3024'
     expect(task.valid?).to be(true)
-    task.due_date = '10/12/2001'
-    expect(task.valid?).to be(false)
   end
 
+  include ActiveSupport::Testing::TimeHelpers
+
   it 'verifies users can update a task whose due date is in the past' do
-    task = Task.new
-    task.description = 'Eat Cheetos'
-    task.due_date = '12/12/2001'
-    expect(task.valid?).to be(true)
+    travel_to 1.year.ago do
+      Task.create(
+        :description => 'Eat Cheetos',
+        :due_date => Date.today,
+        )
+    end
+    expect(Task.valid?).to be(true)
     task.due_date = '10/12/2001'
-    expect(task.valid?).to be(false)
+    expect(Task.valid?).to be(true)
+    task.due_date = '10/12/3001'
+    expect(Task.valid?).to be(true)
   end
 
 
