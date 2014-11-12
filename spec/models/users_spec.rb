cd3e72b922ceba79e9bce63a8597080c1e2521ea
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-12 12:48:46 -0700

    wrote users model spec for case-sensitivity

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,16 +4,16 @@ describe User do
 
   it 'verifies unique email for users' do
     email = "hello@kitty.com"
-    User.create(
-      :email => email,
-      :first_name => "hello",
-      :last_name => "kitty",
-      :password => "hellokitty",
+    User.create!(
+      first_name: "hello",
+      last_name: "kitty",
+      email: email,
+      password: "hellokitty",
       )
     user = User.new(
-      :first_name => "mimi",
-      :last_name => "kitty",
-      :password => "mimikitty",
+      first_name: "mimi",
+      last_name: "kitty",
+      password: "mimikitty",
       )
     expect(user.valid?).to be(false)
     user.email = email
@@ -21,5 +21,26 @@ describe User do
     user.email = "mimi@kitty.com"
     expect(user.valid?).to be(true)
   end
+
+  it 'verifies email is not case-sensitive' do
+    User.create!(
+      first_name: "Bugs",
+      last_name: "Bunny",
+      email: "bugs@bunny.com",
+      password: "carrots",
+    )
+    user = User.new(
+      first_name: "Elmer",
+      last_name: "Fudd",
+      email: "BuGs@buNnY.com",
+      password: "guns",
+    )
+    expect(user.valid?).to be(false)
+    user.email = "BUGS@BUNNY.com"
+    expect(user.valid?).to be(false)
+    user.email = "wabbit@get.com"
+    expect(user.valid?).to be(true)
+  end
+
 
 end
