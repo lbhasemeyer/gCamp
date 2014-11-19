7016637478126b2db0dcd3934ac2abd3d2519c13
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-19 13:50:50 -0700

    breaking up the users _form broke creating and editing users...put it back to how it was

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show, :destroy]
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -13,11 +13,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
-    @user = User.new(params.require(:user).
-        permit(:first_name, :last_name, :email, :password, :password_confirmation))
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to users_path, notice: 'User was successfully created.'
@@ -27,23 +27,21 @@ class UsersController < ApplicationController
   end
 
   def update
-      @user.update(user_params)
+      @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to users_url, notice: 'Project was successfully created'
+        redirect_to users_path, notice: 'Project was successfully created'
       else
         render :edit
       end
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     redirect_to users_url, notice: 'User was successfully deleted.'
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
