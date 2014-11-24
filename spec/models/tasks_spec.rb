2397c94b0995e03c6a96fcd3e602448c60b2e335
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-24 14:32:02 -0700

    tasks model spec - made validity tests more specific

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -5,10 +5,10 @@ describe Task do
   it 'verifies entry of all fields' do
     task = Task.new
     task.valid?
-    expect(task.errors.present?).to eq(true)
+    expect(task.errors[:description].present?).to eq(true)
     task.description = 'Eat Cheetos'
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:description].present?).to eq(false)
     end
 
   it 'verifies date is today or after' do
@@ -16,10 +16,10 @@ describe Task do
     task.description = 'Eat Cheetos'
     task.due_date = '10/12/2001'
     task.valid?
-    expect(task.errors.present?).to eq(true)
+    expect(task.errors[:due_date].present?).to eq(true)
     task.due_date = '12/12/3024'
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:due_date].present?).to eq(false)
   end
 
   include ActiveSupport::Testing::TimeHelpers
@@ -29,24 +29,23 @@ describe Task do
         task.description= 'Eat Cheetos'
         task.due_date= Date.today
         task.valid?
-        expect(task.errors.present?).to eq(false)
-        expect(task.valid?).to be(true)
+        expect(task.errors[:date].present?).to eq(false)
         task.save
     end
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:date].present?).to eq(false)
     task.due_date = '10/12/2001'
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:date].present?).to eq(false)
     task.due_date = '10/12/3001'
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:date].present?).to eq(false)
     task.description = 'Find Pig'
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:date].present?).to eq(false)
     task.complete = "true"
     task.valid?
-    expect(task.errors.present?).to eq(false)
+    expect(task.errors[:date].present?).to eq(false)
   end
 
 end
