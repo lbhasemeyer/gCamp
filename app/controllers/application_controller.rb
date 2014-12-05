b84d4f5b92f9065900390d302662f7221e5f5590
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-05 15:36:12 -0700

    Users should be redirected back to the pages they were trying to access after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,19 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def require_login
-    unless current_user != nil
+  def set_return_point(path, overwrite = false)
+    if overwrite or session[:return_point].blank?
+      session[:return_point] = path
+    end
+  end
+
+  def return_point
+    session[:return_point] || projects_path
+  end
+
+  def require_login(return_point = request.url)
+    unless current_user
+      set_return_point(return_point)
       redirect_to signin_path, notice: "You must be logged in to access that action"
     end
   end
