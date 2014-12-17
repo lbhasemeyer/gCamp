08e9857fecad71acc2e712f72af88532a7468d22
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-17 16:31:52 -0700

    added helper method for headers, fixed broken rspecs from adding all projects dropdown for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  def page_header(title, &block)
+    content_for(:title, title)  
+    html = '<div class="page-header">'.html_safe
+
+    if block
+      html += '<div class ="pull-right">'.html_safe
+      html += capture do
+        block.call
+      end
+      html += '</div>'.html_safe
+    end
+
+    html += '<h1>'.html_safe
+    html += title
+    html += '</h1>'.html_safe
+    html += '</div>'.html_safe
+    html
+  end
+
 end
+
+
+# <div class="page-header">
+#   <div class="pull-right">
+#     <%= link_to 'New Project', new_project_path, class:"btn btn-primary" %>
+#   </div>
+#   <h1>Projects</h1>
+# </div>
