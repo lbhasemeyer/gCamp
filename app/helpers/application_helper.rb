4a58f057ca306fb9e66dc41c15fee51ace682cae
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-17 19:53:00 -0700

    adjusted titles for marketing and application pages - application include | title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
 
   def page_header(title, &block)
-    content_for(:title, title)  
-    html = '<div class="page-header">'.html_safe
+    content_for(:title, title)
 
+    html = '<div class="page-header">'.html_safe
+    
     if block
       html += '<div class ="pull-right">'.html_safe
       html += capture do
